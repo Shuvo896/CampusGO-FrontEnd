569c1fd1c3171aba85d0d285378a5e51aa2b8344
Shuvo <shuvo2305101896@diu.edu.bd>
2025-02-20 19:11:17 +0600

    ami bokaaa

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    const hamburger = document.getElementById('hamburger');
-    sidebar.classList.toggle('active');
-    hamburger.classList.toggle('active');
+  const sidebar = document.querySelector('.sidebar');
+  const hamburger = document.getElementById('hamburger');
+  sidebar.classList.toggle('active');
+  hamburger.classList.toggle('active');
 }
 
 
 function updateDate() {
-    const dateSpan = document.querySelector('.showDate');
-    if (dateSpan) {
-      const now = new Date();
-      const formattedDate = formatDate(now);
-      dateSpan.textContent = formattedDate;
-    }
+  const dateSpan = document.querySelector('.showDate');
+  if (dateSpan) {
+    const now = new Date();
+    const formattedDate = formatDate(now);
+    dateSpan.textContent = formattedDate;
   }
+}
 
 
-  function formatDate(date) {
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    return date.toLocaleDateString(undefined, options);
-  }
-  
-  updateDate();
-  
-  setInterval(updateDate, 60000);
+function formatDate(date) {
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  return date.toLocaleDateString(undefined, options);
+}
+
+updateDate();
+
+setInterval(updateDate, 60000);
