caf100185abbfc8ff9b25f0620a98a34d09948a7
Shuvo <shuvo2305101896@diu.edu.bd>
2025-02-20 14:48:17 +0600

    Added mid contents for the first time please bhai dhorjo dhor egula functional kora lagbeeee

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleSidebar() {
     sidebar.classList.toggle('active');
     hamburger.classList.toggle('active');
 }
+
+
+function updateDate() {
+    const dateSpan = document.querySelector('.showDate');
+    if (dateSpan) {
+      const now = new Date();
+      const formattedDate = formatDate(now);
+      dateSpan.textContent = formattedDate;
+    }
+  }
+
+
+  function formatDate(date) {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString(undefined, options);
+  }
+  
+  updateDate();
+  
+  setInterval(updateDate, 60000);
